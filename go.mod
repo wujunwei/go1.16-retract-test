6584704f5016351987a0e4c1df00ebd78e585c90
adam <wjw3323@live.com>
2021-02-22 17:09:38 +0800

    fix bug

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/wujunwei/go1.16-retract-test
 
 go 1.16
 
-retract v0.2.0
+retract v0.3.0
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package go1_16_retract_test
 
 func Hello() string {
-	return "hello world"
+	return "hello world !"
 }
